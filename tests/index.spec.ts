454171ffa51bc377c06979da2479296db96da8df
ojoquinhaa <jlpradoneiva@gmail.com>
2023-01-29 18:11:48 +0000

    Typescript starter image

---
+++ tests/index.spec.ts
@@ -0,0 +1,11 @@
+import { describe, expect, it } from "@jest/globals";
+
+describe("Testes de teste :)", () => {
+  it("2 + 3 = 5", () => {
+    expect(2 + 3).toBe(5);
+  });
+
+  it("6 + 5 = 12", () => {
+    expect(6 + 5).toBe(12);
+  });
+});
